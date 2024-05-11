787abad39372eeac8bc3e96479aa534ff40a381b
echung32 <143231894+echung32@users.noreply.github.com>
2024-05-10 17:00:58 -1000

    commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='abalone-regression',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict the age of abalone from various physical measurements.',
+    author='echung32',
+    license='MIT',
+)
